0fae2b780e93e3940d407d2c4360ee4b8c714130
Kyle Wood <kyle@denwav.dev>
2021-12-06 20:36:53 -0600

    Day 5, problem 1

---
+++ day05/problem1/source/app.d
@@ -0,0 +1,74 @@
+import std.algorithm;
+import std.ascii;
+import std.conv;
+import std.stdio;
+import std.string;
+
+struct PointPair {
+    Point left;
+    Point right;
+}
+
+struct Point {
+    int x;
+    int y;
+}
+
+Point parseCoords(const string coords) @safe pure {
+    string[] parts = coords.split(",");
+    const Point result = { x: parse!int(parts[0]), y: parse!int(parts[1]) };
+    return result;
+}
+
+PointPair parseLine(const string line) @safe pure {
+    auto parts = line.split(" -> ");
+    const PointPair result = { left: parseCoords(parts[0]), right: parseCoords(parts[1]) };
+    return result;
+}
+
+bool isHorizontalOrVert(const PointPair *pair) {
+    return pair.left.x == pair.right.x || pair.left.y == pair.right.y;
+}
+
+void countPointsBetween(const PointPair *pair, int[Point] *pointCounts) @safe {
+    const int minX = min(pair.left.x, pair.right.x);
+    const int maxX = max(pair.left.x, pair.right.x);
+    const int minY = min(pair.left.y, pair.right.y);
+    const int maxY = max(pair.left.y, pair.right.y);
+
+    for (int i = minX; i <= maxX; i++) {
+        for (int j = minY; j <= maxY; j++) {
+            const Point thisPoint = { x: i, y: j };
+            int *count = thisPoint in *pointCounts;
+            if (count !is null) {
+                *count += 1;
+            } else {
+                (*pointCounts)[thisPoint] = 1;
+            }
+        }
+    }
+}
+
+void main() {
+    auto inputFile = File("input.txt", "r");
+    int[Point] pointCounts;
+
+    foreach (line ; inputFile.byLine) {
+        const auto pair = parseLine(to!string(line));
+        if (!isHorizontalOrVert(&pair)) {
+            continue;
+        }
+
+        countPointsBetween(&pair, &pointCounts);
+    }
+    inputFile.close();
+
+    int count = 0;
+    foreach (i ; pointCounts.byValue) {
+        if (i > 1) {
+            count++;
+        }
+    }
+
+    writeln(count);
+}
